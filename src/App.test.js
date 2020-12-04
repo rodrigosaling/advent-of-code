d7a9767508d458c0ba6d0a6c4286f779d23ccb12
Rodrigo Saling <rodrigosaling@gmail.com>
2020-12-03 22:29:12 -0300

    Renames test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app', () => {
   render(<App />);
   const advent = screen.getByText(/advent of code/i);
   expect(advent).toBeInTheDocument();
