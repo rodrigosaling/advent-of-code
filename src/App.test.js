ea108218ca426c16db2c0e27d75389bbdf484147
Rodrigo Saling <rodrigosaling@gmail.com>
2020-12-03 22:28:14 -0300

    Cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const advent = screen.getByText(/advent of code/i);
+  expect(advent).toBeInTheDocument();
 });
