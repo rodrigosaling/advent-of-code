8674d7d2d4b1767536f9eef77dc966dc2e5493db
Rodrigo Saling <rodrigosaling@gmail.com>
2020-12-07 22:25:42 -0300

    better identification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 
 function App() {
-  const [value, setValue] = useState(`1721
+  const [value0101, setValue0101] = useState(`1721
 979
 366
 299
 675
 1456`);
-  const [answer, setAnswer] = useState('');
+  const [answer0101, setAnswer0101] = useState('');
 
-  const updateInput = (event) => {
-    setValue(event.target.value);
+  const updateInput0101 = (event) => {
+    setValue0101(event.target.value);
   }
 
-  const doTheMagic = (event) => {
+  const doTheMagic0101 = (event) => {
     event.preventDefault();
     const input = document.getElementById('input').value.split('\n');
     const inputLength = input.length;
@@ -24,7 +24,7 @@ function App() {
         const secondValue = parseInt(input[j]);
         // console.log(firstValue + secondValue);
         if (firstValue + secondValue === 2020) {
-          setAnswer(firstValue * secondValue);
+          setAnswer0101(firstValue * secondValue);
         }
       }
     }
@@ -39,10 +39,10 @@ function App() {
       <main>
         <h2>Day 1</h2>
         <h3>Puzzle 1</h3>
-        <form onSubmit={doTheMagic}>
-          <textarea rows="15" value={value} onChange={updateInput} id="input" required/>
+        <form onSubmit={doTheMagic0101}>
+          <textarea rows="15" value={value0101} onChange={updateInput0101} id="input" required/>
           <button type="submit">Submit</button>
-          <input type="text" id="answer" value={answer} readOnly />
+          <input type="text" id="answer" value={answer0101} readOnly />
         </form>
       </main>
     </div>
