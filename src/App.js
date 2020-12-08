07a673d97dd64536a045c2d84dedbf15c5987a39
Rodrigo Saling <rodrigosaling@gmail.com>
2020-12-07 22:40:22 -0300

    adds code for day 1 puzzle 2

--- src/App.js
+++ src/App.js
@@ -15,16 +15,50 @@ function App() {
 
   const doTheMagic0101 = (event) => {
     event.preventDefault();
-    const input = document.getElementById('input').value.split('\n');
+    const input = document.getElementById('input0101').value.split('\n');
     const inputLength = input.length;
 
     for (let i = 0; i<inputLength-1; i++) {
+      const firstValue = parseInt(input[i]);
       for (let j = i+1; j<inputLength; j++) {
-        const firstValue = parseInt(input[i]);
         const secondValue = parseInt(input[j]);
-        // console.log(firstValue + secondValue);
         if (firstValue + secondValue === 2020) {
+          console.log('Values: ', firstValue, secondValue);
           setAnswer0101(firstValue * secondValue);
+        }
+      }
+    }
+  }
+
+  // ----------------------
+
+  const [value0102, setValue0102] = useState(`1721
+979
+366
+299
+675
+1456`);
+  const [answer0102, setAnswer0102] = useState('');
+
+  const updateInput0102 = (event) => {
+    setValue0102(event.target.value);
+  }
+
+  const doTheMagic0102 = (event) => {
+    event.preventDefault();
+    const input = document.getElementById('input0102').value.split('\n');
+    const inputLength = input.length;
+
+    for (let i = 0; i<inputLength-2; i++) {
+      const firstValue = parseInt(input[i]);
+      for (let j = i+1; j<inputLength-1; j++) {
+        const secondValue = parseInt(input[j]);
+        for (let k = j+1; k<inputLength; k++) {
+          const thirdValue = parseInt(input[k]);
+          if (firstValue + secondValue + thirdValue === 2020) {
+            console.log('Values: ', firstValue, secondValue, thirdValue);
+            setAnswer0102(firstValue * secondValue * thirdValue);
+          }
         }
       }
     }
@@ -40,10 +74,20 @@ function App() {
         <h2>Day 1</h2>
         <h3>Puzzle 1</h3>
         <form onSubmit={doTheMagic0101}>
-          <textarea rows="15" value={value0101} onChange={updateInput0101} id="input" required/>
+          <textarea rows="15" value={value0101} onChange={updateInput0101} id="input0101" required/>
           <button type="submit">Submit</button>
           <input type="text" id="answer" value={answer0101} readOnly />
         </form>
+
+        <hr />
+
+        <h3>Puzzle 2</h3>
+        <form onSubmit={doTheMagic0102}>
+          <textarea rows="15" value={value0102} onChange={updateInput0102} id="input0102" required/>
+          <button type="submit">Submit</button>
+          <input type="text" id="answer" value={answer0102} onChange={({target}) => setAnswer0102(target.value)}/>
+        </form>
+
       </main>
     </div>
   );
