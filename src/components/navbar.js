8c82ff12d6c948b3cbfb30b607d802256dc8c049
Rodrigo Saling <rodrigosaling@gmail.com>
2020-12-08 22:48:13 -0300

    adds a navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <div className="navbar-item">
+        <h1 className="">Advent of Code</h1>
+      </div>
+    </div>
+
+    <div className="navbar-menu">
+      <div className="navbar-start">
+        <a className="navbar-item">
+          Home
+        </a>
+
+        <a className="navbar-item">
+          2020
+        </a>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
