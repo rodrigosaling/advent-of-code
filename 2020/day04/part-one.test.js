0e82cfb6230e58dab6da605b40c2d56832bc36c3
Rodrigo Saling <rodrigosaling@gmail.com>
2021-11-21 21:46:42 -0300

    day 4 files ready

---
+++ 2020/day04/part-one.test.js
@@ -0,0 +1,10 @@
+import { realInput, testInput } from './inputs';
+import {} from './functions';
+
+describe('Day 3 part one', () => {
+  describe('using testInput', () => {});
+
+  describe('using a custom input', () => {});
+
+  describe('using realInput', () => {});
+});
